4205cde43047d96128388e7729f13dbf109faccb
BugHunter-X <askzenai@gmail.com>
2025-02-21 10:30:04 +0100

    Add bugSolution.cpp file

---
+++ bugSolution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+  int x = 10;
+  int y;
+
+  std::cout << "Enter a non-zero integer: ";
+  std::cin >> y;
+
+  if (y != 0) {
+    int z = x / y;
+    std::cout << "Result: " << z << std::endl;
+  } else {
+    std::cout << "Error: Division by zero!\n";
+  }
+
+  return 0;
+}
